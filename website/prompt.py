7df80e5fca04dcd197da4afea57d1be72a3d9aad
Chaerim <cofla2020@yonsei.ac.kr>
2024-02-16 14:27:11 +0900

    feat: add result api for sending summary and terms

---
+++ website/prompt.py
@@ -0,0 +1,2 @@
+def request_gemini(news):
+    return {'summary': 'summary of news', 'economic_terminologies': [{'term': 'term1', 'definition': 'definition1'}, {'term': 'term2', 'definition': 'definition2'}]}
--- website/views.py
+++ website/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+from django.http import *
+import json
+from django.http import JsonResponse
+from .crawling import getFullTextfromURL
+from .prompt import request_gemini
 
 # Create your views here.
 
 def website(request):
     return render(request, 'index.html', {'hello': "Hello, World!"})
+
+def answer(request):
+    if request.method == 'POST':
+        url = ""
+        
+        try: 
+            url = json.loads(request.body).get('url')
+        except Exception as e:
+            return HttpResponseBadRequest("Bad Request: url field is missing")
+        
+        if url is None or url == "":
+            return HttpResponseBadRequest("Bad Request: invalid URL")
+        
+        news = getFullTextfromURL(url)
+        result_from_gemini = request_gemini(news)
+        
+        answer = {
+            "news": news,
+            "summary": result_from_gemini['summary'],
+            "economic_terminologies": result_from_gemini['economic_terminologies']
+        }
+        
+        return JsonResponse(answer)
+    
+    else:
+        return HttpResponseBadRequest("Bad Request: Only POST requests are allowed.")
